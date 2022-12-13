c2daa6056e51d70b769b8d18eb402a1a10808df4
«Denis <6545158@gmail.com>
2022-12-13 21:21:21 +0300

    if - else

---
+++ Example 005/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("What is your name?");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "den")
+{
+ Console.WriteLine("Odin with you, Den");
+}
+
+else
+
+{
+     Console.Write("Hello, ");
+     Console.Write(username);
+};
