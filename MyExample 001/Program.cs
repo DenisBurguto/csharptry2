1fa267bece1f04adcc638f2d1c3f3b9896b9ea2a
«Denis <6545158@gmail.com>
2022-12-14 17:24:21 +0300

    solved find the private from two numbers

--- MyExample 001/Program.cs
+++ MyExample 001/Program.cs
@@ -3,35 +3,20 @@ Console.WriteLine("Please input first number");
 string firstNumber = Console.ReadLine();
 Console.WriteLine("Please input second number");
 string secondNumber = Console.ReadLine();
-if (double.TryParse(firstNumber, out var x)) //check if can convert input into number
+if (double.TryParse(firstNumber, out var x) & (double.TryParse(secondNumber, out var y))) //check if can convert input into number
 {
-
-}
-else
-{
-    Console.WriteLine("Wrong input, program is finished");
-}
-if (double.TryParse(secondNumber, out var y)) //check if can convert second input into number
-{
-
-}
-else
-{
-    Console.WriteLine("Wrong input, program is finished");
-
-}
-if (y == 0)
-{
-    Console.WriteLine("It's forbidden to divide with zero!!!");
-}
-else
-{
-    if (x != 0)
+    if (y == 0)
+    {
+        Console.WriteLine("It's forbidden to divide with zero!!!");
+    }
+    else
     {
         double result = x / y;
         Console.WriteLine("the private is");
         Console.WriteLine(result);
     }
-    else { Console.WriteLine("0"); }
 }
-
+else
+{
+    Console.WriteLine("Wrong input, program is finished");
+}
