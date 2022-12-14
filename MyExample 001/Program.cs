f0c106a10b7e4ff5a2518a6945dd090aceaf4245
«Denis <6545158@gmail.com>
2022-12-14 15:31:32 +0300

    try catch

---
+++ MyExample 001/Program.cs
@@ -0,0 +1,30 @@
+﻿
+Console.WriteLine("Please input first number");
+string firstNumber = Console.ReadLine();
+
+    try
+    {
+        int test = Convert.ToInt32(firstNumber);
+        Console.WriteLine("ok, ");
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine("Input string is invalid.");
+    }
+    x = Convert.ToInt32(firstNumber);
+
+
+//Console.WriteLine("Please input second number");
+//string secondNumber = Console.ReadLine();
+//       try {
+//           int y = Convert.ToInt32(secondNumber);
+//          Console.WriteLine("Private (/) is, ");
+//          int numSecond = y;
+//      }
+//      catch (FormatException) {
+//          Console.WriteLine("Input string is invalid.");
+//      }
+//Console.WriteLine(numFirst);        
+//double result = (numFirst / numSecond);
+
+//Console.WriteLine(result);
