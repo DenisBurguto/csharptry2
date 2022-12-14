7a145401b8bdd6a9c3546836622709f1fe3e886d
«Denis <6545158@gmail.com>
2022-12-14 18:45:56 +0300

    task abiut two friends and dog

---
+++ MyExample 002/Program.cs
@@ -0,0 +1,21 @@
+﻿// task about dog and two friends
+double distance = 10000, firstFriendSpeed = 1, secondFriendSpeed = 2, dogSpeed = 5, time = 0;
+int count = 0;
+string friend = "friend1";
+
+while (distance > 10)
+{
+    if (friend == "friend1")
+    {
+        time = distance / (firstFriendSpeed + dogSpeed);
+        friend = "friend2";
+    }
+    else
+    {
+        time = distance / (secondFriendSpeed + dogSpeed);
+    }
+    distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
+    count = count + 1;
+    friend = "friend1";
+}
+Console.WriteLine("Dog will run " + count + " times");
